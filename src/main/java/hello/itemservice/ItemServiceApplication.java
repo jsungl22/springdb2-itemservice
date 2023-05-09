8c9dbcb69e9548380f28756398bb02b269096f66
jsungl <wotjd931026@gmail.com>
2023-05-09 19:46:32 +0900

    테스트 실행시 임베디드 모드 데이터베이스 사용

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -2,11 +2,15 @@ package hello.itemservice;
 
 import hello.itemservice.config.*;
 import hello.itemservice.repository.ItemRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 /**
  * @Import(MemoryConfig.class) : MemoryConfig 를 설정파일로 사용
@@ -17,6 +21,7 @@ import org.springframework.context.annotation.Profile;
 //@Import(JdbcTemplateV2Config.class)
 @Import(JdbcTemplateV3Config.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
+@Slf4j
 public class ItemServiceApplication {
 
 	public static void main(String[] args) {
@@ -39,6 +44,27 @@ public class ItemServiceApplication {
 	@Profile("local")
 	public TestDataInit testDataInit(ItemRepository itemRepository) {
 		return new TestDataInit(itemRepository);
+	}
+
+	/**
+	 * 프로필이 test 인 경우에만 데이터소스를 스프링 빈으로 등록
+	 * 즉, 테스트케이스에서만 이 데이터소스를 스프링 빈으로 등록해서 사용한다
+	 *
+	 * 테스트를 실행할 때 임베디드 모드(메모리 모드)로 동작하는 H2 데이터베이스를 사용 -> 실제 데이터베이스를 사용할 필요 X
+	 * @return
+	 */
+	@Bean
+	@Profile("test")
+	public DataSource dataSource() {
+		log.info("메모리 데이터베이스 초기화");
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.h2.Driver");
+		//jdbc:h2:mem:db 은 임베디드 모드에서 동작하는 H2 데이터베이스를 사용할 수 있다
+		//DB_CLOSE_DELAY=-1 은 임베디드 모드에서 데이터베이스 커넥션 연결이 모두 끊어지는 경우 데이터베이스 종료를 방지하는 설정
+		dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+		dataSource.setUsername("sa");
+		dataSource.setPassword("");
+		return dataSource;
 	}
 
 }
