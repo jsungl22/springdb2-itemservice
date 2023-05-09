878b461e987d2ca802bab259f726a371f5f30f52
jsungl <wotjd931026@gmail.com>
2023-05-09 19:29:26 +0900

    테스트 전용 데이터베이스 사용, 테스트에 트랜잭션 추가

--- src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
+++ src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
@@ -5,9 +5,13 @@ import hello.itemservice.repository.ItemSearchCond;
 import hello.itemservice.repository.ItemUpdateDto;
 import hello.itemservice.repository.memory.MemoryItemRepository;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import java.util.List;
 
@@ -19,12 +23,34 @@ class ItemRepositoryTest {
     @Autowired
     ItemRepository itemRepository;
 
+    //트랜잭션 관련 코드
+    @Autowired
+    PlatformTransactionManager transactionManager; //스프링부트는 자동으로 적절한 트랜잭션 매니저를 스프링 빈으로 등록해준다
+    TransactionStatus status;
+
+    /**
+     * 각각의 테스트 케이스를 실행하기 전에 트랜잭션을 시작한다
+     * 각각의 테스트를 트랜잭션 범위 안에서 실행할 수 있다
+     */
+    @BeforeEach
+    void beforeEach() {
+        //트랜잭션 시작
+        status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+    }
+
+    /**
+     * 각각의 테스트 케이스가 완료된 후 트랜잭션을 롤백한다
+     * 그러면 데이터를 트랜잭션 실행 전 상태로 복구 가능
+     */
     @AfterEach
     void afterEach() {
         //MemoryItemRepository 의 경우 제한적으로 사용
         if (itemRepository instanceof MemoryItemRepository) {
             ((MemoryItemRepository) itemRepository).clearStore();
         }
+
+        //트랜잭션 롤백
+        transactionManager.rollback(status);
     }
 
     @Test
