bf514744ff400cf910bd92ece14cae8a24151449
jsungl <wotjd931026@gmail.com>
2023-05-09 19:50:11 +0900

    add comment

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -46,25 +46,28 @@ public class ItemServiceApplication {
 		return new TestDataInit(itemRepository);
 	}
 
+
 	/**
 	 * 프로필이 test 인 경우에만 데이터소스를 스프링 빈으로 등록
 	 * 즉, 테스트케이스에서만 이 데이터소스를 스프링 빈으로 등록해서 사용한다
 	 *
 	 * 테스트를 실행할 때 임베디드 모드(메모리 모드)로 동작하는 H2 데이터베이스를 사용 -> 실제 데이터베이스를 사용할 필요 X
+	 * 
+	 * 하지만 스프링부트는 이런 임베디드 데이터베이스에 대한 설정도 기본으로 제공하므로 생략가능하다
 	 * @return
 	 */
-	@Bean
-	@Profile("test")
-	public DataSource dataSource() {
-		log.info("메모리 데이터베이스 초기화");
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("org.h2.Driver");
-		//jdbc:h2:mem:db 은 임베디드 모드에서 동작하는 H2 데이터베이스를 사용할 수 있다
-		//DB_CLOSE_DELAY=-1 은 임베디드 모드에서 데이터베이스 커넥션 연결이 모두 끊어지는 경우 데이터베이스 종료를 방지하는 설정
-		dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
-		dataSource.setUsername("sa");
-		dataSource.setPassword("");
-		return dataSource;
-	}
+//	@Bean
+//	@Profile("test")
+//	public DataSource dataSource() {
+//		log.info("메모리 데이터베이스 초기화");
+//		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//		dataSource.setDriverClassName("org.h2.Driver");
+//		//jdbc:h2:mem:db 은 임베디드 모드에서 동작하는 H2 데이터베이스를 사용할 수 있다
+//		//DB_CLOSE_DELAY=-1 은 임베디드 모드에서 데이터베이스 커넥션 연결이 모두 끊어지는 경우 데이터베이스 종료를 방지하는 설정
+//		dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+//		dataSource.setUsername("sa");
+//		dataSource.setPassword("");
+//		return dataSource;
+//	}
 
 }
